8af02a27aceba03f4aec7aa05a7fe25c1366096f
zagirovshamil <zagirovshamil@gmail.com>
2025-05-15 00:44:16 +0300

    fixed some bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Counter } from './components/Counter/Counter'
+import './App.css'
 
 export const App = () => {
   return (
-    <div>Приложение работает очень, очень хорошо!
-
+    <>
+    <div>Приложение работает очень, очень хорошо!</div>
     <Counter />
-
-    </div>
+    </>
   )
 }
