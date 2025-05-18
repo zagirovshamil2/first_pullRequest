9e958564864fcc8f52118b78a00c7b9c81f19c84
zagirovshamil <zagirovshamil@gmail.com>
2025-05-18 23:40:49 +0300

    add variablefor styles and add homepage page with styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
-
+import { HomePage } from '../pages/HomePage'
 import './styles/index.scss'
+
 
 export const App = () => {
   return (
     <>
-    <div>Приложение работает очень, очень хорошо!</div>
+      <div>Приложение работает очень, очень хорошо!</div>
+      <HomePage />
     </>
   )
 }
